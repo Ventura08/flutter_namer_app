a624e9601cb065a2376ad4bb7c073a54afcabf5e
Ventura08 <estevaotevu08@gmail.com>
2023-03-31 17:11:41 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('A random idea:'),
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+              onPressed: () {
+                print('Crico');
+              },
+              child: const Text('Next'))
+        ],
+      ),
+    );
+  }
+}
